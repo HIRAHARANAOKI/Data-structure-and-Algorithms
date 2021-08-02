04bfab3bfea96feac807d102e53b0b72429bf5bc
genenao21@gmail.com <naoki_hirahara@naokinoMacBook-Pro.local>
2021-08-02 15:08:20 +0900

    [add] reverseString

---
+++ cs2/recursion/reverseString/main.py
@@ -0,0 +1,24 @@
+def reverseStringHelper(reversedString, leftString):
+
+    # ベースケース
+    if len(leftString) <= 0:
+        return reversedString
+
+    # print(reversedString, 'reversedString')
+    # print(leftString, 'leftString')
+
+    return reverseStringHelper(leftString[0] + reversedString, leftString[1:])
+
+def reverseString(string):
+   return reverseStringHelper(string[0], string[1:])
+
+
+s = "abcd"
+print(reverseString(s))
+
+
+# "abcd" → ""
+# "abc" → "d"
+# "ab" → "dc"
+# "a" → "dcb"
+# "" → "dcba"

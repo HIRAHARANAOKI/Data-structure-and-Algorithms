eb0f6088a314f7f8bc2b4cc4e56e3bac6cdf58f3
n-hirahara <genenao21@gmail.com>
2021-08-08 10:33:03 +0900

    [add] 'splitAndAdd'

---
+++ cs2/recursion/splitAndAdd/main.py
@@ -0,0 +1,10 @@
+import math
+
+def splitAndAdd(digits):
+    return splitAndAddHelper(digits, 0)
+
+def splitAndAddHelper(digits, sum):
+    if digits <= 0:
+        return sum
+    return splitAndAddHelper(digits/10, sum + math.floor(digits%10))
+print(splitAndAdd(19))

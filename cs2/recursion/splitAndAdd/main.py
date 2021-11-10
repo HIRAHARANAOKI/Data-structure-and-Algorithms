3e3fd9c7824fccae2ff494a37defea2b204f7969
n-hirahara <63541457+HIRAHARANAOKI@users.noreply.github.com>
2021-11-10 11:36:43 +0900

    [add] 数字を分割して足す

--- cs2/recursion/splitAndAdd/main.py
+++ cs2/recursion/splitAndAdd/main.py
@@ -1,12 +1,19 @@
 import math
 
+
 def splitAndAdd(digits):
-    return splitAndAddHelper(digits, 0)
+	return splitAndAddHelper(digits, 0)
+
 
 def splitAndAddHelper(digits, sum):
-    if digits <= 0:
-        return sum
-    return splitAndAddHelper(digits/10, sum + math.floor(digits%10))
+	if digits <= 0:
+		return sum
+
+	return splitAndAddHelper(digits / 10, sum + math.floor(digits % 10))
+
+
 print(splitAndAdd(19))
+print(splitAndAdd(898))
 
-
+print(19 % 10)
+print(math.floor(19 % 10))
